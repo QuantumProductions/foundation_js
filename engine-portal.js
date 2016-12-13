45a9021d2b18dd11cce7fa42f1d3324417108c67
quantumpotato <quantumpotato@gmail.com>
2016-12-13 15:30:05 -0500

    Start recurring time loop with Date.now() for last time

--- engine-portal.js
+++ engine-portal.js
@@ -1,17 +1,54 @@
-function time(last, acc) {
-	if (acc > 1000) {
-		console.log("delta" + delta);
-	}
-
+function time(last, acc, structs, msgs) {
   let now = Date.now();
   let delta = now - last;
   let total = acc + delta;
-  if (total > 1000) {
-  	console.log("accumulated 1000");
-  	window.requestAnimationFrame(time.bind(null, now, 0));
+  if (total > 1000) { //Reloop
+  	let input = gamepadInputs();
+  	window.requestAnimationFrame(time.bind(null, now, total - 1000, structs, msgs));
   } else {
-  	window.requestAnimationFrame(time.bind(null, now, total));
+  	window.requestAnimationFrame(time.bind(null, now, total, structs, msgs));
   }
 }
 
-window.requestAnimationFrame(time.bind(null, 0, 0));
+window.requestAnimationFrame(time.bind(null, Date.now(), 0, {}, []));
+
+function gamepadInputs() {
+	var pads = navigator.getGamepads();
+
+	pads.forEach(function (gp, i) {
+
+	});
+	for (let gp in pads) {
+		if (!gp) {
+			break;
+		}
+
+	}
+
+}
+
+function mappedInputs(pads, index, mappedPads) {
+	if (pads.length == 0) {
+		return mapped;
+	}
+
+	let gp = pads.pop();
+
+ 	let mappedPad = mappedPadInputs(gp, index);
+
+	return mappedInputs(pads, index + 1, mappedPads.concat([mappedPad]));
+}
+
+function mappedPadInputs(gp, index) {
+	let buttonMap = [1, 2]; //maps for each gamepad slot
+	return mappedPadButtonInputs(gp.buttons, buttonMap, {});	 //Buttons only for now
+}
+
+function mappedPadButtonInputs(unmappedButtons, buttonMap, mappedButtons) {
+	if (buttonMap.length == 0) {
+		return mappedButtons;
+	}
+
+	mappedButtons[buttonMap.pop()] = unmappedButtons.pop().value;
+	return mappedPadButtonInputs(unmappedButtons, buttonMap, mappedButtons);
+}
