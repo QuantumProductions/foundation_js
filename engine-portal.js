047db5448d763573dc4219f465b4c612220425e9
quantumpotato <quantumpotato@gmail.com>
2016-12-15 18:54:32 -0500

    Recur time with structs + msgs value from step

--- engine-portal.js
+++ engine-portal.js
@@ -6,8 +6,8 @@ function time(last, acc, structs, msgs) {
   	let input = gamepadInputs();
   	console.log("input" + JSON.stringify(input));
   	msgs.concat([{input: input}]);
-  	structs2 = step(structs, msgs);
-  	window.requestAnimationFrame(time.bind(null, now, total - 100, structs, msgs));
+  	let s2m2 = step(structs, msgs);
+  	window.requestAnimationFrame(time.bind(null, now, total - 100, s2m2.structs, s2m2.msgs));
   } else {
   	window.requestAnimationFrame(time.bind(null, now, total, structs, msgs));
   }
@@ -15,6 +15,7 @@ function time(last, acc, structs, msgs) {
 
 function step(structs, msgs) {
 	//{}, [], {}	
+	return {structs: structs, msgs: msgs};
 }
 
 window.requestAnimationFrame(time.bind(null, Date.now(), 0, {}, []));
